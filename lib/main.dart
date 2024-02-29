b402dd3c3886a8853c33f2262a422243700c429a
ANGEL_MANUEL_GOMEZ_HERNANDEZ <a.21308051281222@cbtis128.edu.mx>
2024-02-29 20:23:34 +0000

    actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: '',
       theme: ThemeData(
-        // useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MipaginaInicialState();
+}
+
+class _MipaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Walmart",
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "Tu",
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                text: "Comp",
+                icon: Icon(Icons.shopping_bag),
+              ),
+              Tab(
+                text: "Prod",
+                icon: Icon(Icons.shopping_basket),
+              ),
+              Tab(
+                text: "Car",
+                icon: Icon(Icons.shopping_cart),
+              ),
+              Tab(
+                text: "Noti",
+                icon: Icon(Icons.notification_add),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(
+              Icons.person,
+              size: 350,
+              color: Color(0xff0044ff),
+            ),
+            Icon(Icons.shopping_bag, size: 350, color: Color(0xc2993dfb)),
+            Icon(Icons.shopping_basket, size: 350, color: Color(0xc23d56fb)),
+            Icon(Icons.shopping_cart, size: 350, color: Color(0xc23dacfb)),
+            Icon(Icons.notification_add, size: 350, color: Color(0xc27f3dfb)),
+          ],
         ),
       ),
     );
